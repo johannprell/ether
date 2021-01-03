61d0e74e9522b1ca397d55c0a50bc4442195631b
Johann Prell <johann.prell@gmail.com>
2021-01-03 11:07:29 +0100

    start on demo 00 - some ui scripts for ether

---
+++ Assets/Ether/Demos/Shared/Scripts/SetCarrierFromSlider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Ether;
+
+public class SetCarrierFromSlider : MonoBehaviour
+{
+    public Slider slider;
+    public Carrier carrier;
+    void Update()
+    {
+        carrier.value = slider.value;
+    }
+}
---
+++ Assets/Ether/Scripts/_EffectorComponents/EffectorSetTextValue.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Ether;
+
+public class EffectorSetTextValue : MonoBehaviour, IEffector
+{
+    public Text target;
+
+    public void PerformEffect(float value)
+    {
+        target.text = value.ToString("0.00");
+    }
+}
